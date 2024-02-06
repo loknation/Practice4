2811f674b6af1e87ee53604c267cc519ea93ac3d
loknation <pierce.noltemeyer@talawanda.org>
2024-02-06 13:53:03 -0500

    Module 4 practice

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,45 @@ public class Main {
     }
     static int SumOf10(){
         int total = 0;
-        for (int i = 1; i < 10; i++){
+        for (int i = 1; i <= 10; i++){
             total += i;
         }
         return total;
     }
     static int ProductOf10(){
-        int total = 0;
-        for (int i = 1; i < 10; i++){
+        int total = 1;
+        for (int i = 1; i <= 10; i++){
             total *= i;
         }
         return total;
     }
-
+    static void Interest(){
+        System.out.println(1000*1.05);
+        System.out.println(1000*1.05*1.05);
+        System.out.println(1000*1.05*1.05*1.05);
+    }
+    static void PrintName(){
+        String[] piercePatern = {
+                "****   *****   *****   ***      *   *****",
+                "*   *    *     *       *  *   *     *",
+                "****     *     *****   **     *     *****",
+                "*        *     *       * *    *     *",
+                "*      *****   *****   *  *     *   *****"
+        };
+        for (int i = 0; i<5; i++){
+            System.out.println(piercePatern[i]);
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("1.");
+        HelloWorld();
+        System.out.println("2.");
+        System.out.println(SumOf10());
+        System.out.println("3.");
+        System.out.println(ProductOf10());
+        System.out.println("4.");
+        Interest();
+        System.out.println("5.");
+        PrintName();
+    }
 }
